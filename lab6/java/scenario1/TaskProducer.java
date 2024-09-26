871db8bc90ded0a3c47e321618529a6421490ff6
ArthurAntunes3 <arthur.silva@ccc.ufcg.edu.br>
2024-09-26 17:38:27 -0300

    TaskProducer

--- lab6/java/scenario1/TaskProducer.java
+++ lab6/java/scenario1/TaskProducer.java
@@ -1,8 +1,33 @@
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicLong;
+
 public class TaskProducer implements Runnable {
+    private final long ProducerID;
+    private final BlockingQueue<Task> filaDeTasks;
+    private final AtomicLong tarefaID;
+
+    public TaskProducer(long ProducerID, BlockingQueue<Task> filadeTasks){
+        this.ProducerID = ProducerID;
+        this.filaDeTasks = filadeTasks;
+        this.tarefaID = new AtomicLong(0);
+    }
+
+    public long getProducerID() {
+        return ProducerID;
+    }
 
     @Override
     public void run() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'run'");
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                Task task = new Task(tarefaID.incrementAndGet());
+                Thread.sleep(5000);
+                System.out.println("task " + tarefaID + "produzida pelo TaskProducer:" + ProducerID);
+                filaDeTasks.put(task);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break;
+            }
+        }
     }
 }
